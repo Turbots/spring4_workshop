841fd49169b999974eca9fe3dbd99b74702888ba
Tim De Grande <git@de-grande.be>
2015-02-18 21:32:39 +0100

    Base version

--- src/main/java/be/ordina/workshop/spring4/components/RealMessageService.java
+++ src/main/java/be/ordina/workshop/spring4/components/RealMessageService.java
@@ -6,7 +6,6 @@ import be.ordina.workshop.spring4.model.ChatMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Description;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -22,7 +21,6 @@ public class RealMessageService implements MessageService {
     private Collection<ChatMessage> messages = new ArrayList<>();
 
     @Autowired
-    @Lazy
     private LazyService lazyService;
 
     @PostConstruct
